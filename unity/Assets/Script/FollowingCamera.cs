5d620ea619e74086ee6b8c2a0cab2db54c5024fc
Pathoscross <pathoscross@gmail.com>
2016-03-06 04:58:11 +0800

    鏡頭跟隨
    鏡頭跟隨

--- unity/Assets/Script/FollowingCamera.cs
+++ unity/Assets/Script/FollowingCamera.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour
 {
-    public Transform m_Target = null;
+    private Transform m_Target = null;
 
     private void Start()
     {
@@ -11,10 +11,16 @@ public class FollowingCamera : MonoBehaviour
 
     private void Update()
     {
+        CameraControl(this.transform.position);
     }
 
     private void CameraControl(Vector3 CameraPos)
     {
+        if (m_Target == null)
+        {
+            m_Target = ObjectPool.m_Instance.FindNowPlayer().transform;
+        }
+
         float x = m_Target.position.x;
         float y = this.transform.position.y;
         float z = this.transform.position.z;
