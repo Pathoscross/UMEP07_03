7bc8b4194abeb7f252fdf106077287e76253c254
kohanchu <yojufutari@hotmail.com>
2016-03-14 23:14:24 +0800

    3/14場景&主角
    3/14場景&主角

---
+++ unitySubject/Assets/Blade_NPC_SpecialPack/scripts/Girl_2_Move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Girl_2_Move : MonoBehaviour
+{
+    public Animator Anim;
+    public AnimatorStateInfo BS;
+    private bool x;
+
+    private static int AttacStandy = Animator.StringToHash("Base.Layer.BG_AttackStandy");
+    private static int Run = Animator.StringToHash("Base.Layer.BG_Run01");
+    private static int L_Run = Animator.StringToHash("Base.Layer.BG_L_Run01");
+    private static int R_Run = Animator.StringToHash("Base.Layer.BG_R_Run01");
+    private static int B_Run = Animator.StringToHash("Base.Layer.BG_B_Run01");
+    private static int Attac = Animator.StringToHash("Base.Layer.BG_Attack00");
+    private static int Death = Animator.StringToHash("Base.Layer.BG_Death");
+
+    // Use this for initialization
+    private void Start()
+    {
+        x = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Anim.SetBool("Run", false);
+        Anim.SetBool("L_Run", false);
+        Anim.SetBool("R_Run", false);
+        Anim.SetBool("B_Run", false);
+        Anim.SetBool("Attac", false);
+        Anim.SetBool("Death", false);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Anim.SetBool("Run", x);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Anim.SetBool("L_Run", x);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Anim.SetBool("R_Run", x);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Anim.SetBool("B_Run", x);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            Anim.SetBool("Attac", x);
+        }
+    }
+}
---
+++ unitySubject/Assets/Blade_NPC_SpecialPack/scripts/boymove.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boymove : MonoBehaviour
+{
+
+    public Animator Anim;
+    public AnimatorStateInfo BS;
+    bool x;
+
+    static int AttacStandy = Animator.StringToHash("Base.Layer.BW_AttackStandy");
+    static int Run = Animator.StringToHash("Base.Layer.BG_Run01");
+    static int L_Run = Animator.StringToHash("Base.Layer.BW_L_Run01");
+    static int R_Run = Animator.StringToHash("Base.Layer.BW_R_Run01");
+    static int B_Run = Animator.StringToHash("Base.Layer.BW_B_Run01");
+    static int Attac = Animator.StringToHash("Base.Layer.BW_Combo1_1");
+    static int Death = Animator.StringToHash("Base.Layer.BG_Death");
+    // Use this for initialization
+    void Start()
+    {
+        x = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Anim.SetBool("Run", false);
+        Anim.SetBool("L_Run", false);
+        Anim.SetBool("R_Run", false);
+        Anim.SetBool("B_Run", false);
+        Anim.SetBool("Attac", false);
+        if (Input.GetKey(KeyCode.W))
+        {
+            Anim.SetBool("Run", x);
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Anim.SetBool("L_Run", x);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Anim.SetBool("R_Run", x);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Anim.SetBool("B_Run", x);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            Anim.SetBool("Attac", x);
+        }
+
+    }
+}
